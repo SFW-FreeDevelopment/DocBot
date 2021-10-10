449d9e7a88aa27f506dd1ea391c390707d087a6c
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-10 01:40:08 -0400

    Added docs command

---
+++ DocBot.App/Commands/DocsCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DocBot.App.Commands
+{
+    public class DocsCommand : CommandBase
+    {
+        [Command("docs")]
+        public async Task HandleCommandAsync()
+        {
+            await ReplyAsync($"**W3 Schools**{Environment.NewLine}" +
+                             $"  • HTML - https://www.w3schools.com/tags/default.asp{Environment.NewLine}" +
+                             $"  • CSS - https://www.w3schools.com/css/default.asp{Environment.NewLine}" +
+                             $"  • JavaScript - https://www.w3schools.com/js/default.asp{Environment.NewLine}" +
+                             $"  • SQL - https://www.w3schools.com/sql/default.asp{Environment.NewLine}" +
+                             $"**Direct Sources**{Environment.NewLine}" +
+                             $"  • C# - https://docs.microsoft.com/en-us/dotnet/api/?view=netcore-3.1");
+        }
+    }
+}
